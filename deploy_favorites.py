b91f07c979d2ca61efc8d5a4388ef2d793d61e5e
Michael Francis <michaelmugendi1707@gmail.com>
2025-12-14 12:33:08 +0300

    Update: deployed contract

--- deploy_favorites.py
+++ deploy_favorites.py
@@ -2,16 +2,27 @@ import boa
 from boa.network import NetworkEnv, EthereumRPC
 from dotenv import load_dotenv
 import os
+from eth_account import Account
 
 load_dotenv()
 
 def main():
-    rpc_url = os.getenv("RPC_URL")
     print("Read the vyper code and deploy the contract using anvil.")
+
+    rpc_url = os.getenv("RPC_URL", "http://127.0.0.1:8545")
+
+    env = NetworkEnv(EthereumRPC(rpc_url))
+
+    boa.set_env(env)
+
+    my_account = Account.from_key(os.getenv("ANVIL_PRIVATE_KEY"))
+
+    boa.env.add_account(my_account, force_eoa=True)   
+
     favorites_contract = boa.load("favorites.vy")
 
-    starting_favorite_number = favorites_contract.retrieve()
-    print(f"Starting favorite number is: {starting_favorite_number}")
+    # starting_favorite_number = favorites_contract.retrieve()
+    
     
     # print("Deployment complete.")
 
